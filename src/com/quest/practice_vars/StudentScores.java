17978a1a479fc011edb66f016ceb1897aeb3c431
devikab03 <devikab312@gmail.com>
2024-11-14 20:37:51 +0530

    day7

--- src/com/quest/practice_vars/StudentScores.java
+++ src/com/quest/practice_vars/StudentScores.java
@@ -31,13 +31,15 @@ public class StudentScores {
     public static void calculateAverageScores(String[] names, int[] ages, int[][] scores) {
         System.out.println("Average scores of Students:");
 
-        for (int i = 0; i < names.length; i++) {
+        for (int i = 0; i < scores.length; i++) {
             int sum = 0;
             int count = 0;
+            //calculating the sum
             for (int score : scores[i]) {
                 sum += score;
                 count++;
             }
+            //finding the average
             double average = (double) sum / count;
             System.out.println("Name of Student:" + names[i]);
             System.out.println("Age of Student:" + ages[i]);
@@ -47,54 +49,80 @@ public class StudentScores {
 
     public static void findHighestScorer(String[] names, int[] ages, int[][] scores) {
         System.out.println("Highest scorer:");
-        int highest = 0;
-        String topScorer = "";
-        for (int i = 0; i < names.length; i++) {
+        double highest = 0;
+        String topScorer = " ";
+        for (int i = 0; i < scores.length; i++) {
             int Sum = 0;
             int count = 0;
+            //calculating the sum
             for (int score : scores[i]) {
                 Sum += score;
                 count++;
             }
-
+            //finding the average
             double average = (double) Sum / count;
             if (average > highest) {
                 highest = (int) average;
                 topScorer = names[i];
             }
-            System.out.println("Top scorer:" + topScorer+"Highest average"+highest);
         }
+        System.out.println("Top scorer:" + topScorer + "\nAverage" + highest);
+
     }
 
     public static void rankStudents(String[] names, int[] ages, int[][] scores) {
-        System.out.println("Ranking Students:");
-        double[] average = new double[names.length];
+        double[] average = new double[scores.length];
 
-        for (int i = 0; i < names.length; i++) {
+        for (int i = 0; i < scores.length; i++) {
             int sum = 0;
             int count = 0;
+            //calculating the sum
             for (int score : scores[i]) {
                 sum += score;
                 count++;
             }
             average[i] = (double) sum / count;
-            for(i=0;i<average.length-1;i++) {
-                for (int j=0;j<average.length-i-1;j++){
-                    if (average[j]>average[j+1]) {
-                        double temp = average[j];
-                        average[j] = average[j+1];
-                        average[j+1] = temp;
-
-
-                    }
+        }
+        //use bubble sort for sorting averagescore
+        for (int i = 0; i < average.length - 1; i++) {
+            for (int j = 0; j < average.length - i - 1; j++) {
+                if (average[j] < average[j + 1]) {//sorted in descending order
+                    //swap average score
+                    double temp = average[j];
+                    average[j] = average[j + 1];
+                    average[j + 1] = temp;
+                    //swap names
+                    String tempName = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = tempName;
+                    //swap age
+                    int tempage = ages[j];
+                    ages[j] = ages[j + 1];
+                    ages[j + 1] = tempage;
                 }
             }
-
         }
-
-
+        System.out.println("Ranked Students:");
+        for (int i = 0; i < average.length; i++) {
+            System.out.println("Rank " + (i + 1) + " " + names[i] + " AverageScore " + average[i] + " Age :" + ages[i]);
+        }
     }
+
 
     public static void detectMissingData(String[] names, int[][] scores) {
+        System.out.println("Detect missing data:");
+        for (int i = 0; i < scores.length; i++) { //loop over each student
+            for (int j = 0; j < scores[i].length; j++) {//loop over each subject for current student
+                if (scores[i][j] == -1) {
+                    System.out.println("Missing data at index " + j);
+                    break;
+
+                }
+            }
+        }
     }
 }
+
+
+
+
